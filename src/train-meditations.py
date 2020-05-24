a6d46820593439931902ea16f840592519c5bf01
saj7 <scott2000jones@gmail.com>
2020-05-24 19:34:41 +0100

    trained to 0.9 loss on meditations

--- src/train-meditations.py
+++ src/train-meditations.py
@@ -1,5 +1,6 @@
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn('../weights/meditations.hdf5')
-textgen.train_from_file('../data/meditations.txt', num_epochs=1)
+#textgen = textgenrnn('textgenrnn_weights.hdf5')
+textgen.train_from_file('../data/meditations.txt', num_epochs=50)
 textgen.save('../weights/meditations.hdf5')
